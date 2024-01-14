e4ca5a9b64d099ba4ad6e29c83381a31648ae29f
Guro2019 <gurami.pantskhava@gmail.com>
2024-01-14 12:03:39 +0400

    new one comit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 // Global app controller
+
+import Search from "./modules/Search";
+import { elements } from "./views/base";
+
+
+/* 
+serch object
+Current recipe object
+Shoping list object
+liked recipe
+*/
+const state = {};
+
+/* serch controler */
+
+const controlSearch = (e) => {
+    e.prevenDefault()
+
+
+    state.search = new Search("pizza")
+    console.log(state)
+}
+
+// 1) get query from view
+
+// 2) 
+
+
+elements.searchForm.addEventListener("submit", controlSearch)
